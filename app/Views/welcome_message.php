1773b0a3ca7549a950c5b2896f0fe077d48a05f2
Lanzz-tech <mhdlangka@gmail.com>
2026-07-28 18:11:03 +0700

    p

--- app/Views/welcome_message.php
+++ app/Views/welcome_message.php
@@ -305,7 +305,7 @@
 
     <div class="copyrights">
 
-        <p>&copy; <?= date('Y') ?> CodeIgniter Foundation. CodeIgniter is open source project released under the MIT
+        <p>&copy; <?= date('Y') ?> CodeIgniter Foundation 4. CodeIgniter is open source project released under the MIT
             open source licence.</p>
 
     </div>
